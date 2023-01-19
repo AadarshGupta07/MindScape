fedb85899675882fc5b83d719503622450cad645
AadarshGupta07 <103953164+AadarshGupta07@users.noreply.github.com>
2023-01-19 17:47:08 +0530

    Created Static Responsive Home Page

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+const lines = document.getElementById('lines')
+const slide = document.getElementById('slide')
+const cross = document.getElementById('cross')
+cross.classList.add('hide')
+lines.classList.add('unhide')
+
+lines.addEventListener('click', () => {
+    slide.style.transform = 'translateX(0)'
+    cross.classList.remove('hide')
+    lines.classList.add('hide')
+})
+cross.addEventListener('click', () => {
+    slide.style.transform = 'translateX(100%)'
+    lines.classList.remove('hide')
+    cross.classList.add('hide')
+})
+
+
+
+
